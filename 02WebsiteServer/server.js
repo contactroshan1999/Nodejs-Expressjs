933e1a31f5cc1f212d3953f8e1a6debc30e29882
Roshan <rk123517@gmail.com>
2023-04-02 18:03:26 +0530

    Create your local server to host your website

---
+++ 02WebsiteServer/server.js
@@ -0,0 +1,54 @@
+const http = require("http");
+const url = require("url");
+const fs= require("fs");
+const path = require("path");
+const { unescape } = require("querystring");
+const hostName ="127.0.0.1";
+const port = 5000;
+const mimeTypes = 
+{
+    'html':'text/html',
+    'css':'text/css',
+    'js':'text/javascript',
+    'png':'image/png',
+    'jpeg':'image/jpeg',
+    'jpg':'image/jpg'
+};
+
+http.createServer((req,res) => {
+
+    var myuri = url.parse(req.url).pathname
+    var filename = path.join(process.cwd(),unescape(myuri));
+    console.log(filename);
+    var loadFile;
+    try{
+        loadFile=fs.lstatSync(filename);
+    }catch(error)
+    {
+        res.writeHead(404,{'Content-Type':'text/plain'});
+        res.write('404 Page Not Found');
+        res.end();
+        return;
+    }
+
+    if(loadFile.isFile())
+    {
+        var mimeType = mimeTypes[path.extname(filename).split('.').reverse()[0]];
+        res.writeHead(200,{'Content-Type':mimeType});
+        var filestream = fs.createReadStream(filename);
+        filestream.pipe(res);
+    }
+    else if(loadFile.isDirectory())
+    {
+        res.writeHead(302,{"Location":"index.html"});
+        res.end();
+    }
+    else
+    {
+        res.writeHead(500,{'Content-Type':'text/plain'});
+        res.write('500 Internal Server Error');
+        res.end();
+    }
+}).listen(port,hostName, () =>{
+    console.log(`Server is running at ${port}`);
+})
